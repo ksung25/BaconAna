f1fc603eaf1e316532dd3704b25e628652cb4b67
Kevin Kai Hong Sung <ksung@cern.ch>
2014-12-07 12:16:36 +0100

    store tightish trimming

--- DataFormats/interface/TAddJet.hh
+++ DataFormats/interface/TAddJet.hh
@@ -14,7 +14,7 @@ namespace baconhep
         pt_p2(0),ptraw_p2(0),eta_p2(0),phi_p2(0),mass_p2(0),area_p2(0),
         pt_t1(0),ptraw_t1(0),eta_t1(0),phi_t1(0),mass_t1(0),area_t1(0),
         pt_t2(0),ptraw_t2(0),eta_t2(0),phi_t2(0),mass_t2(0),area_t2(0),
-        //pt_t3(0),ptraw_t3(0),eta_t3(0),phi_t3(0),mass_t3(0),area_t3(0),
+        pt_t3(0),ptraw_t3(0),eta_t3(0),phi_t3(0),mass_t3(0),area_t3(0),
         pt_t4(0),ptraw_t4(0),eta_t4(0),phi_t4(0),mass_t4(0),area_t4(0),
 	pt_m1(0),ptraw_m1(0),eta_m1(0),phi_m1(0),mass_m1(0),area_m1(0),
 	pt_m2(0),ptraw_m2(0),eta_m2(0),phi_m2(0),mass_m2(0),area_m2(0),
@@ -31,7 +31,7 @@ namespace baconhep
     float pt_p2,ptraw_p2,eta_p2,phi_p2,mass_p2,area_p2;    // Jet Info with pruning Tight 
     float pt_t1,ptraw_t1,eta_t1,phi_t1,mass_t1,area_t1;    // Jet Info with trimming default   
     float pt_t2,ptraw_t2,eta_t2,phi_t2,mass_t2,area_t2;    // Jet Info with trimming Tight 
-    //float pt_t3,ptraw_t3,eta_t3,phi_t3,mass_t3,area_t3;    // Jet Info with trimming Tightish
+    float pt_t3,ptraw_t3,eta_t3,phi_t3,mass_t3,area_t3;    // Jet Info with trimming Tightish
     float pt_t4,ptraw_t4,eta_t4,phi_t4,mass_t4,area_t4;    // Jet Info with trimming Small cone
     float pt_m1,ptraw_m1,eta_m1,phi_m1,mass_m1,area_m1;    // Jet Info with filtering default   
     float pt_m2,ptraw_m2,eta_m2,phi_m2,mass_m2,area_m2;    // Jet Info with fitlering Tight 
